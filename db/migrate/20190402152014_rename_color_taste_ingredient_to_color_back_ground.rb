dd4c301b5571e93d0c0bb3d3448de09daf942e2f
Chris Camacho <cacamacho91@gmail.com>
2019-04-03 16:18:33 +0100

    changed the migrations for colors

--- db/migrate/20190402152014_rename_color_taste_ingredient_to_color_back_ground.rb
+++ db/migrate/20190402152014_rename_color_taste_ingredient_to_color_back_ground.rb
@@ -1,6 +1,8 @@
 class RenameColorTasteIngredientToColorBackGround < ActiveRecord::Migration[5.2]
   def change
-    rename_column :tastes, :color, :color_background
-    rename_column :ingredients, :color, :color_background
+    rename_column :tastes, :colorBackground, :color_background
+    rename_column :ingredients, :colorBackground, :color_background
+    rename_column :tastes, :colorText, :color_text
+    rename_column :ingredients, :colorText, :color_text
   end
 end
