91d1cbc9c39bdbbe7e69e44196cd4432442fefa9
Chris Camacho <cacamacho91@gmail.com>
2019-04-11 21:24:50 +0100

    added user info to return of login function

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,7 +29,13 @@ class Api::V1::UsersController < ApplicationController
   def login
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
-      render json: {id: @user.id, username: @user.username, token: issue_token({id: @user.id})}
+      render json: {
+        id: @user.id, 
+        username: @user.username, 
+        token: issue_token({id: @user.id}),
+        myCreationIds: @user.creation_ids,
+        myStarredIds: @user.starred_ids
+      }
     else
       render json: {error: "Username/Password combination invalid."}, status: 401
     end
